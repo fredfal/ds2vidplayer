6c12f1f4ad2878eb9eabdafccb2e8f4ea85574c5
Fred Fal <fredfal@-g-m-a-i-l.com>
2013-07-07 13:32:01 +0200

    Commit rattrapage

--- src/screen.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SCREEN_H_
-#define _SCREEN_H_
-
-#include "image.h"
-
-int load_image_file(int screen_id, const char* archive_filename, const char* filename);
-int load_image_data(int screen_id, image_data_s *image_data);
-
-#endif
